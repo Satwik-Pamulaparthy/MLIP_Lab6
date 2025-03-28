7d567ac96535a9b0ec35eba6e86b1055ad2aa397
Satwik-Pamulaparthy <pamulaparthysatwik@gmail.com>
2025-03-27 19:47:15 -0500

    All tests passing: completed test_data_split

--- test_utility.py
+++ test_utility.py
@@ -1,34 +1,36 @@
 import pytest
 import pandas as pd
 import numpy as np
-from prediction_demo import data_preparation,data_split,train_model,eval_model
+from prediction_demo import data_preparation, data_split, train_model, eval_model
 
 @pytest.fixture
 def housing_data_sample():
     return pd.DataFrame(
-      data ={
-      'price':[13300000,12250000],
-      'area':[7420,8960],
-    	'bedrooms':[4,4],	
-      'bathrooms':[2,4],	
-      'stories':[3,4],	
-      'mainroad':["yes","yes"],	
-      'guestroom':["no","no"],	
-      'basement':["no","no"],	
-      'hotwaterheating':["no","no"],	
-      'airconditioning':["yes","yes"],	
-      'parking':[2,3],
-      'prefarea':["yes","no"],	
-      'furnishingstatus':["furnished","unfurnished"]}
+        data={
+            'price': [13300000, 12250000],
+            'area': [7420, 8960],
+            'bedrooms': [4, 4],
+            'bathrooms': [2, 4],
+            'stories': [3, 4],
+            'mainroad': ["yes", "yes"],
+            'guestroom': ["no", "no"],
+            'basement': ["no", "no"],
+            'hotwaterheating': ["no", "no"],
+            'airconditioning': ["yes", "yes"],
+            'parking': [2, 3],
+            'prefarea': ["yes", "no"],
+            'furnishingstatus': ["furnished", "unfurnished"]
+        }
     )
 
 def test_data_preparation(housing_data_sample):
     feature_df, target_series = data_preparation(housing_data_sample)
-    # Target and datapoints has same length
-    assert feature_df.shape[0]==len(target_series)
 
-    #Feature only has numerical values
-    assert feature_df.shape[1] == feature_df.select_dtypes(include=(np.number,np.bool_)).shape[1]
+    # Target and datapoints have same length
+    assert feature_df.shape[0] == len(target_series)
+
+    # Feature only has numerical values
+    assert feature_df.shape[1] == feature_df.select_dtypes(include=(np.number, np.bool_)).shape[1]
 
 @pytest.fixture
 def feature_target_sample(housing_data_sample):
@@ -36,6 +38,15 @@ def feature_target_sample(housing_data_sample):
     return (feature_df, target_series)
 
 def test_data_split(feature_target_sample):
-    return_tuple = data_split(*feature_target_sample)
-    # TODO test if the length of return_tuple is 4
-    raise NotImplemented
+    X_train, X_test, y_train, y_test = data_split(*feature_target_sample)
+
+    # Check if function returns 4 elements
+    assert len([X_train, X_test, y_train, y_test]) == 4
+
+    # Check total length remains same
+    assert len(X_train) + len(X_test) == len(feature_target_sample[0])
+    assert len(y_train) + len(y_test) == len(feature_target_sample[1])
+
+    # Check if X and y are aligned
+    assert len(X_train) == len(y_train)
+    assert len(X_test) == len(y_test)
